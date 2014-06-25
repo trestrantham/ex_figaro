a7117287cf786523b871dc937f3380a6e86f8815
Tres Trantham <tres@trestrantham.com>
2014-06-25 02:12:38 -0400

    Initial load and get ENV functionality

--- lib/figaro.ex
+++ lib/figaro.ex
@@ -1,2 +1,39 @@
 defmodule Figaro do
+  def load do
+    config_file
+    |> :yamerl_constr.file
+    |> List.flatten
+    |> Enum.each(fn var -> System.put_env(format_env_key(var), format_env_value(var)) end)
+  end
+
+  def get(key) do
+    key
+    |> to_string
+    |> String.upcase
+    |> System.get_env
+  end
+
+  defp config_file do
+    file = File.cwd!
+           |> Path.join("config")
+           |> Path.join("application.yml")
+
+    cond do
+      File.exists?(file) -> file
+      true               -> raise "application.yml not found"
+    end
+  end
+
+  defp format_env_key(env_key) do
+    env_key
+    |> elem(0)
+    |> to_string
+    |> String.upcase
+  end
+
+  defp format_env_value(env_value) do
+    env_value
+    |> elem(1)
+    |> to_string
+  end
 end
--- test/figaro_test.exs
+++ test/figaro_test.exs
@@ -1,3 +1,25 @@
 defmodule FigaroTest do
   use ExUnit.Case, async: true
+
+  setup_all do
+    File.cd! "test/fixtures/project"
+
+    Figaro.load
+  end
+
+  teardown_all do
+    System.delete_env("FOO")
+    System.delete_env("BAR")
+  end
+
+  test "parsing config.yml file" do
+    assert Figaro.get("foo") == "foo"
+    assert Figaro.get("bar") == "bar"
+  end
+
+  test "loading into system environment" do
+
+    assert System.get_env("FOO") == "foo"
+    assert System.get_env("BAR") == "bar"
+  end
 end
